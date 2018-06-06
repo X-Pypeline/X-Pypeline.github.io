5190bfeb56c2bd46c9782623839ffb34e1f4496a
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2018-06-06 17:47:34 -0500

    Alright we have some waveform examples

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '0+untagged.72.g8cdbf6e.dirty',
+    VERSION: '0+untagged.81.g737ed9e.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
