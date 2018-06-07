a852967f46478e6d176d46829c6bbd282bf9c243
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2018-06-07 12:58:06 -0500

    A working example of replicating xinjectsignal.m, I have not demonstrated the actual injection into the noise using the very easy to use gwpy.inject method, but seemingly form xdetection that is the next thing that happens once you have appropraitely projected the signal

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '0+untagged.81.g737ed9e.dirty',
+    VERSION: '0+untagged.83.g753277f.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
