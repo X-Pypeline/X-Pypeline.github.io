567fe6ea767b18415e5ba5a118b5bc921615fe6a
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2018-06-11 09:13:56 -0500

    Alright these docs have some more content and represent a couple bug fixes.

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '0+untagged.83.g753277f.dirty',
+    VERSION: '0+untagged.85.gcf75ef0.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
