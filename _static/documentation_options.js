13b510dc0039f8974c4149fea4f6782d4be72bf7
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2018-07-18 14:55:20 -0500

    Update to docs to reflect some new logic in code, some of docs are broken but i do not want to wati much longer

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '0+untagged.85.gcf75ef0.dirty',
+    VERSION: '0+untagged.94.gd8906ca.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
