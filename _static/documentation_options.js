b69221d41a6ef6ece3fe57dd3e22ad7ab954077c
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2018-07-25 16:33:38 -0500

    Alright the injection and reading of some real colored gaussian noise happens

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '0+untagged.94.gd8906ca.dirty',
+    VERSION: '0+untagged.97.gafaa967.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
